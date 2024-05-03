e821f325d2c02e7f2c2c2e79b8e4c2a7b535b963
akshat jain <chromeblood@akshats-MacBook-Air-3.local>
2024-05-03 08:33:12 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Create_room from "./create_room";
+import Join_room from './join_room_1.js';
+import Message_rev from "./message_recived";
+import './Moulikcss/button.css';
+import './Moulikcss/coolinput.css'
+import './Moulikcss/modifyButtons.css';
+import './Moulikcss/roomCodeText.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import "./App.css"
 
-function App() {
+const Disposable_Forms = () => {
+  const [press, setpress] = useState(false);
+  const [press_1, setpress_1] = useState(false);
+
+  if (press || press_1) {
+    if (press) {
+      return (
+        <div>
+          <Create_room/>
+        </div>
+      );
+    }
+    if (press_1) {
+      return (
+        <div>
+          <Join_room/>
+        </div>
+      );
+    }
+  }
+
+  const Handleclick = () => {
+    setpress(true);
+  };
+  
+  const Handleclick_1 = () => {
+    setpress_1(true);
+  };
+
   return (
-   <div>
-    not
-   </div>
+
+    <div class="bg-dark ">
+    <div className="container">
+      <button className="button" onClick={Handleclick}>
+        Create room
+      </button>
+      <button className="button" onClick={Handleclick_1}>
+        Join Room
+      </button>
+    </div>
+    </div>
   );
 }
 
-export default App;
+export default Disposable_Forms;
